ab28315e3ca97d21b08e09c30e23fdd6482cb0f6
nicolecasares <nicolecasares@gmail.com>
2018-10-02 15:09:57 -0700

    switch statments

---
+++ switch-statments/app.js
@@ -0,0 +1,20 @@
+const hogwartsHouse = '';
+
+
+switch(hogwartsHouse){
+  case 'Gryffindor':
+    console.log(`10 ponts to ${hogwartsHouse}`);
+    break;
+  case 'Hufflepuff':
+    console.log( 'What\'s so special about you guys again');
+    break;
+  case 'Ravenclaw':
+    console.log( 'Can you help me with my homework?');
+    break;
+  case 'Slytherin':
+    console.log( 'ummm please don\' hurt me');
+    break;
+  default:
+    console.log('Must me a muggle then')
+}
+
