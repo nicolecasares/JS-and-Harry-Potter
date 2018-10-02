f9598c7b8140072eefb331d47ba7b1c9b362569d
conniepocky <c.amer@icloud.com>
2018-10-02 21:05:16 +0100

    added files

---
+++ arrow-functions/app.js
@@ -0,0 +1,21 @@
+//Harry Potter and Arrow Functions
+
+//Variables
+
+let students = []
+
+// CODE
+
+function add_student_function(house, name) {
+    students.append([name, student])
+}
+
+//The code below does the same as the code above
+
+let add_student = (house, name) => students.append([name, student])
+
+//Testing
+
+console.log(add_student_function("Slytherin", "Severus Snape"))
+
+console.log(add_student("Slytherin", "Severus Snape"))
