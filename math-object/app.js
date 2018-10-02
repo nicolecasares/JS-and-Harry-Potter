409688809444c41bb6636d66316bbbbe94a92686
nicolecasares <nicolecasares@gmail.com>
2018-10-02 10:46:41 -0700

    Math Object

---
+++ math-object/app.js
@@ -0,0 +1,32 @@
+//harry potter July 31, 1980
+//Hermione Jean Granger 19 September 1979
+//Ron Bilius Weasley  March 1, 1980
+
+
+let val;
+
+const today = new Date();
+let harryBirthday = new Date('7-31-1980 11:25:00');
+let hermineBirthday = new Date('September 19 1979');
+let ronBirthday = new Date('3/1/1980');
+
+val = today.getMonth();
+val = today.getDate();
+val = today.getDay();
+val = today.getFullYear();
+val = today.getHours();
+val = today.getMinutes();
+val = today.getSeconds();
+val = today.getMilliseconds();
+val = today.getTime();
+
+let = lunaBirthday = new Date();
+
+lunaBirthday.setMonth(1);
+lunaBirthday.setDate(13);
+lunaBirthday.setFullYear(1981);
+lunaBirthday.setHours(3);
+lunaBirthday.setMinutes(30);
+lunaBirthday.setSeconds(25);
+
+console.log(lunaBirthday);
